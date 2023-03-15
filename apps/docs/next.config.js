86f01e6125425ca6318c781ad1aba290f6695e34
Tai Bui <tai.c.bui@gmail.com>
2023-03-16 05:39:24 +0700

    update

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,5 +2,5 @@ module.exports = {
   reactStrictMode: false,
   basePath: process.env.BASE_PATH || '',
   assetPrefix: process.env.BASE_PATH || '',
-  transpilePackages: ['wc-compass'],
-};
+  transpilePackages: ['@comfortdelgro/wc-compass'],
+}
