0f513493c50d28b6fce4bfbf2233fdec34dca2d2
Tai Bui <tai.c.bui@gmail.com>
2023-03-16 06:16:18 +0700

    Setup build & release pipeline

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ['wc-compass'],
-  rewrites: async () => [
-    {
-      source: '/public/index.html',
-      destination: '/pages/api/index.js',
-    },
-  ],
-};
+  reactStrictMode: false,
+  basePath: process.env.BASE_PATH || '',
+  assetPrefix: process.env.BASE_PATH || '',
+  transpilePackages: ['@comfortdelgro/wc-compass'],
+}
