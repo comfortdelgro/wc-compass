19a1e0e1d2c8b22640461c3340eea297e23822e7
fpt-duongtx <fpt-duongtx@users.noreply.github.com>
2023-03-16 08:11:11 +0000

    deploy: 03ee7f837148488648a8cee97226a90ac584f2b9

---
+++ _next/static/zCJDjvIQLAMpBLuaFf-0H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-97e35848544fe8f7.js"],"/_error":["static/chunks/pages/_error-beeb60f079cb2abc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
