39f765d4c72b7a91dfe11fb504a098221fc07935
TuDQ <quoctu2014@gmail.com>
2023-03-20 08:26:57 +0700

    Update logic for table component

--- packages/wc-compass/src/layouts/table/table-body.js
+++ packages/wc-compass/src/layouts/table/table-body.js
@@ -1,11 +1,74 @@
 export class CdgTableBody extends HTMLElement {
-  constructor() {
-    super();
-    this.setAttribute('role','tbody')
+  get checkable() {
+    return this.hasAttribute('checkable')
   }
+
+  set checkable(value) {
+    if (value) {
+      const rows = this.querySelectorAll('cdg-table-row')
+      for (let index = 0; index < rows.length; index++) {
+        const row = rows.item(index)
+        this.createCheckboxCell(row)
+      }
+      this.setAttribute('checkable', '')
+    } else {
+      this.removeAttribute('checkable')
+    }
+  }
+
+  static get observedAttributes() {
+    return ['checkable']
+  }
+
+  constructor() {
+    super()
+    this.setAttribute('role', 'tbody')
+  }
+
   connectedCallback() {
-    this.classList.add('cdg-talbe-body');
+    this.classList.add('cdg-table-body')
+  }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (oldValue === newValue) return
+    this[attr] = this.hasAttribute(attr)
+  }
+
+  createCheckboxCell(tableHeadRowElement) {
+    if (!this.checkboxCellElement) {
+      const checkboxCellElement = document.createElement('cdg-table-cell')
+      checkboxCellElement.classList.add(
+        'cdg-table-cell',
+        'cdg-table-cell-checkbox',
+      )
+      const container = document.createElement('div')
+      container.classList.add('cdg-table-checkbox-contaner')
+      const checkboxContainer = document.createElement('label')
+      checkboxContainer.classList.add('cdg-checkbox')
+      const checkboxElement = document.createElement('input')
+      checkboxElement.type = 'checkbox'
+      checkboxElement.classList.add('cdg-cell-checkbox')
+      checkboxContainer.appendChild(checkboxElement)
+      checkboxElement.addEventListener('change', (event) =>
+        this.handleCheckboxChange(tableHeadRowElement, event),
+      )
+      container.appendChild(checkboxContainer)
+      checkboxCellElement.appendChild(container)
+      tableHeadRowElement.prepend(checkboxCellElement)
+    }
+  }
+
+  handleCheckboxChange(row, event) {
+    const checked = !!event.target.checked
+    if (checked) {
+      row.setAttribute('checked', '')
+    } else {
+      row.removeAttribute('checked')
+    }
+    if (event.bubbles) {
+      this.dispatchEvent(
+        new CustomEvent('onRowCheck', {detail: {checked: checked}}),
+      )
+    }
   }
 }
-
-
--- packages/wc-compass/src/layouts/table/table-row.js
+++ packages/wc-compass/src/layouts/table/table-row.js
@@ -1,10 +1,9 @@
 export class CdgTableRow extends HTMLElement {
   constructor() {
-    super();
-    this.setAttribute('role','tr')
+    super()
+    this.setAttribute('role', 'tr')
   }
   connectedCallback() {
-    this.classList.add('cdg-talbe-row');
+    this.classList.add('cdg-table-row')
   }
 }
-
