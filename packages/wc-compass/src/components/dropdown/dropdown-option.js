dcd17d3cfe40dda67acd5bde184e2c1add3e87bf
fpt-tom-du <tudq3@fsoft.com.vn>
2023-03-22 09:35:23 +0700

    Add logic find select item to display

--- packages/wc-compass/src/components/dropdown/dropdown-option.js
+++ packages/wc-compass/src/components/dropdown/dropdown-option.js
@@ -1,61 +1,62 @@
 export class CdgDropdownOption extends HTMLElement {
-  colorElement;
-  tickElement;
-  multiple = false;
+  colorElement
+  tickElement
+  multiple = false
+  displaySelect
 
   static get observedAttributes() {
-    return ['selected', 'current-color'];
+    return ['selected', 'current-color']
   }
 
   constructor() {
-    super();
-    this.tickElement = document.createElement('cdg-icon');
-    this.tickElement.setAttribute('name', 'tick');
-    this.tickElement.setAttribute('size', '10');
-    this.multiple = this.hasAttribute('multiple');
+    super()
+    this.tickElement = document.createElement('cdg-icon')
+    this.tickElement.setAttribute('name', 'tick')
+    this.tickElement.setAttribute('size', '16')
+    this.multiple = this.hasAttribute('multiple')
   }
 
   connectedCallback() {
-    this.classList.add('cdg-dropdown-option');
+    this.classList.add('cdg-dropdown-option')
   }
 
   attributeChangedCallback(attr, oldValue, newValue) {
-    if (oldValue === newValue) return;
+    if (oldValue === newValue) return
     switch (attr) {
       case 'selected':
         if (newValue) {
-          this.classList.add('cdg-dropdown-option-selected');
+          this.classList.add('cdg-dropdown-option-selected')
           if (!this.multiple) {
             if (this.getAttribute('current-color')) {
-              this.color.appendChild(this.tickElement);
+              this.color.appendChild(this.tickElement)
             } else {
-              this.tickElement.classList.add('cdg-icon-color-dropdown');
-              this.appendChild(this.tickElement);
+              this.tickElement.classList.add('cdg-icon-color-dropdown')
+              this.appendChild(this.tickElement)
             }
           }
         } else {
           if (!this.multiple) {
             if (this.getAttribute('current-color')) {
-              this.color.removeChild(this.tickElement);
+              this.color.removeChild(this.tickElement)
             } else {
-              this.removeChild(this.tickElement);
+              this.removeChild(this.tickElement)
             }
           }
-          this.classList.remove('cdg-dropdown-option-selected');
+          this.classList.remove('cdg-dropdown-option-selected')
         }
-        break;
+        break
       case 'current-color':
         if (newValue) {
-          this.classList.add('cdg-dropdown-option-flex');
-          this.color = document.createElement('div');
-          this.color.classList.add('cdg-dropdown-option-color');
-          this.color.style.backgroundColor = newValue;
-          this.appendChild(this.color);
+          this.classList.add('cdg-dropdown-option-flex')
+          this.color = document.createElement('div')
+          this.color.classList.add('cdg-dropdown-option-color')
+          this.color.style.backgroundColor = newValue
+          this.appendChild(this.color)
         }
-        break;
+        break
 
       default:
-        break;
+        break
     }
   }
 }
