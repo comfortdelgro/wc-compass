7cc3e3099e9519b33e22da9c47a78de088e3eb0e
Hai Do Tien <dotienhai1611@gmail.com>
2023-03-24 13:20:54 +0700

    add text editor icons, modify toolbar

--- packages/wc-compass/src/components/rich-text-editor/rte-toolbar.js
+++ packages/wc-compass/src/components/rich-text-editor/rte-toolbar.js
@@ -1,23 +1,74 @@
 export class CdgRichTextEditorToolbar extends HTMLElement {
-  static get observedAttributes() {
-    return ['editor']
-  }
-
-  get editor() {
-    return this.hasAttribute('editor')
-  }
   constructor() {
     super()
+    // const viewMoreButton = document.createElement(button);
+    // viewMoreButton.innerHTML = 
   }
 
+  set editor(value) {
+    this._editor = value
+  }
+  get editor() {
+    return this._editor
+  }
+  buttonList
   connectedCallback() {
-    const buttonList = document.querySelectorAll('button.cdg-rte-buttons')
-    if (buttonList && buttonList.length) {
-      buttonList.forEach((button) => {
+    this.buttonList = document.querySelectorAll('button.cdg-rte-buttons')
+    this.editor.on('transaction', ({editor, transaction}) => {
+      // The editor state has changed.
+      if (this.buttonList.length) {
+        this.checkActive()
+      }
+    })
+    if (this.buttonList && this.buttonList.length) {
+      this.buttonList.forEach((button) => {
         button.addEventListener('click', () => {
-          console.log(button.getAttributeNames())
+          const detail = this.getEventData(button)
+          const {operation, data} = detail
+          this.editor.chain().focus()[operation](data).run()
         })
       })
     }
+
+    window.addEventListener('resize', (event) => {
+      if(this.scrollHeight > this.clientHeight) {
+        
+      }
+      
+    })
+  }
+
+  getEventData(button) {
+    const attributes = {}
+    button
+      .getAttributeNames()
+      .forEach((name) => (attributes[name] = button.getAttribute(name)))
+    const {operation, level, alignment, name} = attributes
+    let detail = {operation, name}
+    switch (operation) {
+      case 'toggleHeading':
+        detail.data = {
+          level: +level,
+        }
+        break
+      case 'setTextAlign':
+        detail.data = alignment
+        break
+
+      // TODO: link and image
+    }
+
+    return detail
+  }
+
+  checkActive() {
+    this.buttonList.forEach((button) => {
+      const {name, data} = this.getEventData(button)
+      if (this.editor.isActive(name, data)) {
+        button.classList.add('active')
+      } else {
+        button.classList.remove('active')
+      }
+    })
   }
 }
