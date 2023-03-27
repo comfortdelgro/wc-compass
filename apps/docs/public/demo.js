d3816d7a54468c140266ef514f16446e3ca78a1e
michael-le-19 <minhlt19@fsoft.com.vn>
2023-03-27 17:41:50 +0700

    wip: save progess

--- apps/docs/public/demo.js
+++ apps/docs/public/demo.js
@@ -39,6 +39,7 @@ import './components/sub-nav.html'
 import './components/table.html'
 import './components/tabs.html'
 import './components/tag-box.html'
+import './components/templates/dashboard.html'
 import './components/text-field.html'
 import './components/timeline.html'
 import './components/toast.html'
@@ -102,6 +103,7 @@ const contentMap = {
   rangeSlider: './components/range-slider.html',
   tagBox: './components/tag-box.html',
   video: './components/video.html',
+  dashboardTemplate: './components/templates/dashboard.html'
 }
 
 const documentContent = [
@@ -330,6 +332,17 @@ const documentContent = [
       {
         name: 'Toast',
         slug: 'toast',
+      },
+    ],
+  },
+  {
+    name: 'Templates',
+    id: 'template',
+    icon: 'signature',
+    children: [
+      {
+        name: 'Dashboard',
+        slug: 'dashboardTemplate',
       },
     ],
   },
