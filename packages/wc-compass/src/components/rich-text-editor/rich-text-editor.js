c67242c430cdf29a8ea71b2dde5b93495a4284d4
Hai Do Tien <dotienhai1611@gmail.com>
2023-04-06 17:20:03 +0700

    add features to rte, add examples

--- packages/wc-compass/src/components/rich-text-editor/rich-text-editor.js
+++ packages/wc-compass/src/components/rich-text-editor/rich-text-editor.js
@@ -1,4 +1,4 @@
-import {Editor} from '@tiptap/core'
+import { Editor } from '@tiptap/core'
 import Color from '@tiptap/extension-color'
 import TextStyle from '@tiptap/extension-text-style'
 
@@ -11,7 +11,11 @@ import TextAlign from '@tiptap/extension-text-align'
 import Underline from '@tiptap/extension-underline'
 import StarterKit from '@tiptap/starter-kit'
 
+
 export class CdgRichTextEditor extends HTMLElement {
+  static get observedAttributes() {
+    return ['content']
+  }
   editor
 
   constructor() {
@@ -48,6 +52,13 @@ export class CdgRichTextEditor extends HTMLElement {
     const toolbar = this.querySelector('cdg-rte-toolbar')
     if (toolbar) {
       toolbar.editor = this.editor
+    }
+  }
+
+  attributeChangedCallback() {
+    const content = this.getAttribute('content')
+    if (!!content && typeof content === 'string') {
+      this.editor.commands.setContent(content)
     }
   }
 }
