17cbbae8c94fb24270a0804f2e382731fbf39ffa
DuongTX <duongtx@fsoft.com.vn>
2023-04-12 16:59:54 +0700

    Update docs for progress, avatar, gutter

---
+++ apps/docs/public/components/gutter/properties/properties.section.js
@@ -0,0 +1,33 @@
+import {CdgBaseComponent} from '../../../shared/base-component'
+
+export class CdgGutterPropertiesSection extends CdgBaseComponent {
+  constructor() {
+    super()
+    this.htmlContent = `<section class="guideline-section">
+    <h3 class="sample-section-title">Props & Methods</h3>
+  
+    <cdg-info-table id="propertiesTable"></cdg-info-table>
+  </section>
+    `
+  }
+
+  onInit() {
+    const table = document.querySelector('#propertiesTable')
+    table.options = PROPERTIES_TABLE_OPTIONS
+
+    table.data = [
+      {
+        name: '<code>place</code>',
+        type: '<code>left</code> | <code>right</code>',
+        default: '<code>right</code>',
+        description: "The side that you want to resize of it's parent.",
+      },
+      {
+        name: '<code>use-collapse</code>',
+        type: '<code>property</code>',
+        default: "<code>''</code>",
+        description: 'To display quick collapse icon button on top of gutter.',
+      },
+    ]
+  }
+}
---
+++ apps/docs/public/components/progress/properties/properties.section.js
@@ -0,0 +1,46 @@
+import {CdgBaseComponent} from '../../../shared/base-component'
+
+export class CdgProgressPropertiesSection extends CdgBaseComponent {
+  constructor() {
+    super()
+    this.htmlContent = `<section class="guideline-section">
+    <h3 class="sample-section-title">Props & Methods</h3>
+  
+    <cdg-info-table id="propertiesTable"></cdg-info-table>
+  </section>
+    `
+  }
+
+  onInit() {
+    const table = document.querySelector('#propertiesTable')
+    table.options = PROPERTIES_TABLE_OPTIONS
+
+    table.data = [
+      {
+        name: '<code>percentage</code>',
+        type: '<code>number</code>',
+        default: '<code>indeterminate</code>',
+        description:
+          'Value of percentage from <code>0</code> to <code>100</code>. If you leave it without percentage, it will displays as indeterminate mode',
+      },
+      {
+        name: '<code>size</code>',
+        type: "<code>''</code> | <code>medium</code> | <code>large</code> | <code>xlarge</code>",
+        default: "<code>''</code>",
+        description: 'To show progress bar smaller or larger.',
+      },
+      {
+        name: '<code>rounded</code>',
+        type: '<code>property</code>',
+        default: "<code>''</code>",
+        description: 'To round the progress corners.',
+      },
+      {
+        name: '<code>color</code>',
+        type: 'Color string. Ex: <code>color="#107c10"</code>',
+        default: "<code>''</code>",
+        description: 'To set the color of current percentage bar.',
+      },
+    ]
+  }
+}
