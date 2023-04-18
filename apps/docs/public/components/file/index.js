350661c5d8dddac782e6acae214651053b9108e8
fpt-tom-du <tudq3@fsoft.com.vn>
2023-04-18 13:37:09 +0700

    Add guidelines for card-blogs, dialog, file, file-upload

---
+++ apps/docs/public/components/file/index.js
@@ -0,0 +1,8 @@
+import {CdgFilePropertiesSection} from './properties/properties.section'
+import {CdgFileStylesSection} from './styles/styles.section'
+
+customElements.define('cdg-file-properties-section', CdgFilePropertiesSection)
+
+customElements.define('cdg-file-styles-section', CdgFileStylesSection)
+
+export {CdgFilePropertiesSection}
