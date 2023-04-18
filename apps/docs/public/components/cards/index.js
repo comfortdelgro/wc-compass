8a2fb1c9d3bd4a93d2ad45e8009d8187f6e3f632
fpt-tom-du <tudq3@fsoft.com.vn>
2023-04-18 08:44:59 +0700

    Add table styles guidelines

--- apps/docs/public/components/cards/index.js
+++ apps/docs/public/components/cards/index.js
@@ -1,8 +1,10 @@
 import {CdgCardCoverPropertiesSection} from './cover/cover.section'
 import {CdgCardHeaderPropertiesSection} from './header/header.section'
 import {CdgCardsPropertiesSection} from './properties/properties.section'
+import {CdgCardStylesSection} from './styles/styles.section'
 
 customElements.define('cdg-cards-properties-section', CdgCardsPropertiesSection)
+customElements.define('cdg-card-styles-properties-section', CdgCardStylesSection)
 customElements.define(
   'cdg-card-header-properties-section',
   CdgCardHeaderPropertiesSection,
