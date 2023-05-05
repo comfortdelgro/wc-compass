56e57d514a4b0fec8ae930c90fbaf22d1c5d5921
DuongTX <duongtx@fsoft.com.vn>
2023-05-05 16:18:05 +0700

    Update for documentation project

--- apps/docs/public/demo.js
+++ apps/docs/public/demo.js
@@ -1,7 +1,6 @@
 export * from './components/index'
 export * from './fragments/index'
 export * from './pages/index'
-export * from './portal/index'
 export * from './services/index'
 
 import {SampleSection} from './sample-section'
