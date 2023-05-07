b11a31240bef5122a4a7b7f8ca7bbf4ed5859d06
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-05-07 23:16:08 +0700

    [yagin][completed support nested data]

--- apps/docs/public/components/table/index.js
+++ apps/docs/public/components/table/index.js
@@ -2,6 +2,7 @@ import './table.html'
 
 import {CdgTableEventsSection} from './events/events.section'
 import {CdgTableLayoutSection} from './layouts/layout.section'
+import {CdgTableSimpleSectionNested} from './nested-data/simple.section.nested'
 import {CdgTableOptionsSection} from './options/options.section'
 import {CdgTablePropertiesSection} from './properties/properties.section'
 import {CdgTableSimpleSection} from './simple/simple.section'
@@ -9,6 +10,10 @@ import {CdgTableDemo} from './table'
 
 customElements.define('cdg-table-options-section', CdgTableOptionsSection)
 customElements.define('cdg-table-simple-section', CdgTableSimpleSection)
+customElements.define(
+  'cdg-table-simple-section-nested',
+  CdgTableSimpleSectionNested,
+)
 customElements.define('cdg-table-layout-section', CdgTableLayoutSection)
 customElements.define('cdg-table-properties-section', CdgTablePropertiesSection)
 customElements.define('cdg-table-events-section', CdgTableEventsSection)
@@ -17,6 +22,7 @@ customElements.define('cdg-table-demo', CdgTableDemo)
 export {
   CdgTableOptionsSection,
   CdgTableSimpleSection,
+  CdgTableSimpleSectionNested,
   CdgTableLayoutSection,
   CdgTablePropertiesSection,
   CdgTableEventsSection,
