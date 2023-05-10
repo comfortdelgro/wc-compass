e3ac0e2b57b249a2d28ab967a03531fa05ff8880
DuongTX <duongtx@fsoft.com.vn>
2023-05-10 12:55:03 +0700

    Fix for checkboxes on Firefox

--- apps/docs/public/components/checkboxes/checkboxes.js
+++ apps/docs/public/components/checkboxes/checkboxes.js
@@ -6,24 +6,4 @@ export class CdgChexboxesDemo extends CdgDocumentComponent {
     super()
     this.template = template
   }
-
-  onInit() {
-    const checkbox1 = this.querySelector('#indeterminate')
-    checkbox1.setAttribute('indeterminate', '')
-
-    const checkbox2 = (this.querySelector(
-      '#roundedIndeterminate',
-    ).indeterminate = true)
-    checkbox2.setAttribute('indeterminate', '')
-
-    const checkbox3 = (this.querySelector(
-      '#disabledIndeterminate',
-    ).indeterminate = true)
-    checkbox3.setAttribute('indeterminate', '')
-
-    const checkbox4 = (this.querySelector(
-      '#roundedDisabledIndeterminate',
-    ).indeterminate = true)
-    checkbox4.setAttribute('indeterminate', '')
-  }
 }
