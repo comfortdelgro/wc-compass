916727c2a4ab8cb3e3bb6f88dc53c38b03798c51
fpt-tom-du <125246230+fpt-tom-du@users.noreply.github.com>
2023-05-23 17:03:18 +0700

    Change slider mechanism (#159)

--- packages/wc-compass/src/layouts/card/card-header.js
+++ packages/wc-compass/src/layouts/card/card-header.js
@@ -1,34 +1,36 @@
 export class CdgCardHeader extends HTMLElement {
   get title() {
-    return this.getAttribute('title');
+    return this.getAttribute('title')
   }
 
   set title(title) {
-    this.setAttribute('title', title);
+    this.setAttribute('title', title)
   }
 
-  titleElement;
+  titleElement
 
   constructor() {
-    super();
+    super()
   }
 
   static get observedAttributes() {
-    return ['title'];
+    return ['title']
   }
 
   connectedCallback() {
-    this.classList.add('cdg-card-header');
-    this.titleElement = document.createElement('h3');
-    this.titleElement.classList.add('cdg-card-title');
-    this.titleElement.textContent = this.title;
-    this.prepend(this.titleElement);
+    this.classList.add('cdg-card-header')
+    if (!this.titleElement) {
+      this.titleElement = document.createElement('h3')
+      this.titleElement.classList.add('cdg-card-title')
+      this.titleElement.textContent = this.title
+      this.prepend(this.titleElement)
+    }
   }
 
   attributeChangedCallback(attr, oldValue, newValue) {
     if (attr === 'title') {
       if (this.titleElement) {
-        this.titleElement.textContent = newValue;
+        this.titleElement.textContent = newValue
       }
     }
   }
