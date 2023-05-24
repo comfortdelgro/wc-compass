f81f05d286207b252f605d98d972c83d2086ac16
DuongTX <duongtx@fsoft.com.vn>
2023-05-24 15:05:35 +0700

    Add icon font into project

--- packages/wc-compass/src/components/icon/icon.js
+++ packages/wc-compass/src/components/icon/icon.js
@@ -1,6 +1,35 @@
 import {CdgIconSize} from '../../shared/core.js'
 import {downloadSVGContent, toLowerCaseAndDash} from '../../shared/utilities'
 
+const MULTIPLE_COLORS_ICON = [
+  'browser',
+  'bug',
+  'calendar',
+  'zigLogo',
+  'chartLine',
+  'chartPie',
+  'compassLogo',
+  'grids',
+  'hourglass',
+  'hatWinzard',
+  'lightbulb',
+  'mobBus',
+  'mobBuscar',
+  'mobRoadTax',
+  'notification',
+  'openExternal',
+  'orgChart',
+  'organization',
+  'password',
+  'settings',
+  'sparkles',
+  'spinner',
+  'status',
+  'tableColumn',
+  'wLogo',
+  'zigLogo',
+]
+
 export class CdgIcon extends CdgIconSize {
   static get observedAttributes() {
     return ['name', 'size']
@@ -45,7 +74,16 @@ export class CdgIcon extends CdgIconSize {
     super.attributeChangedCallback(attr, oldValue, newValue)
     switch (attr) {
       case 'name':
-        this.fetchAndShowIcon()
+        if (MULTIPLE_COLORS_ICON.includes(this.name)) {
+          this.fetchAndShowIcon()
+        } else {
+          if (oldValue) {
+            this.classList.remove('icon-' + toLowerCaseAndDash(oldValue))
+          }
+          if (this.name) {
+            this.classList.add('icon-' + toLowerCaseAndDash(this.name))
+          }
+        }
         break
 
       default:
