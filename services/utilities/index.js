49fbc9da0a33f47c3ad463b7f81268c3a87a9f18
Tai-Chi-Bui <Tai-Chi-Bui@users.noreply.github.com>
2023-06-07 04:04:56 +0000

    deploy: 0088b5fe2c4065ddf4d7d515bc2a96f419cb5e9c

---
+++ services/utilities/index.js
@@ -0,0 +1,7 @@
+import {CdgUtilitiesDemo} from './utilities'
+import {CdgUtilitySection} from './utility/utility.section'
+
+customElements.define('cdg-utilities-demo', CdgUtilitiesDemo)
+customElements.define('cdg-utility-section', CdgUtilitySection)
+
+export {CdgUtilitiesDemo, CdgUtilitySection}
