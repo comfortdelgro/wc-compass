a11ff03d09ba878e5669581d797fd7816678f7dc
fpt-duongtx <119040724+fpt-duongtx@users.noreply.github.com>
2023-06-12 21:55:08 +0700

    CDS-287: Add textarea style and demo (#176)

---
+++ apps/docs/public/components/textarea/textarea.js
@@ -0,0 +1,9 @@
+import {CdgDocumentComponent} from '../../shared/document-component'
+import template from './textarea.html'
+
+export class CdgTextareaDemo extends CdgDocumentComponent {
+  constructor() {
+    super()
+    this.template = template
+  }
+}
