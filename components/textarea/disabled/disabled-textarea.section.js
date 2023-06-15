ae026c4ac46215ad731db7e3fce1118e29d5c979
Tai-Chi-Bui <Tai-Chi-Bui@users.noreply.github.com>
2023-06-15 03:00:13 +0000

    deploy: cd66163519356b8e71bc035642a70a1a388678c1

---
+++ components/textarea/disabled/disabled-textarea.section.js
@@ -0,0 +1,9 @@
+import {CdgBaseComponent} from '../../../shared/base-component'
+import template from './disabled-textarea.section.html'
+
+export class CdgDisabledTextareaSection extends CdgBaseComponent {
+  constructor() {
+    super()
+    this.template = template
+  }
+}
