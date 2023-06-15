2483bcbe3a0d931fb2ea6d084f25a05f35506ddf
fpt-duongtx <duongtx@fpt.com>
2023-06-15 19:11:00 +0700

    Add common regex

--- apps/docs/public/services/utilities/index.js
+++ apps/docs/public/services/utilities/index.js
@@ -1,7 +1,25 @@
+import {CdgRegEmailSection} from './reg-email/reg-email.section'
+import {CdgRegNumberSection} from './reg-number/reg-number.section'
+import {CdgRegPhoneSection} from './reg-phone/reg-phone.section'
+import {CdgRegSpaceSection} from './reg-space/reg-space.section'
+import {CdgRegTextSection} from './reg-text/reg-text.section'
 import {CdgUtilitiesDemo} from './utilities'
 import {CdgUtilitySection} from './utility/utility.section'
 
 customElements.define('cdg-utilities-demo', CdgUtilitiesDemo)
 customElements.define('cdg-utility-section', CdgUtilitySection)
+customElements.define('cdg-reg-space-section', CdgRegSpaceSection)
+customElements.define('cdg-reg-text-section', CdgRegTextSection)
+customElements.define('cdg-reg-email-section', CdgRegEmailSection)
+customElements.define('cdg-reg-phone-section', CdgRegPhoneSection)
+customElements.define('cdg-reg-number-section', CdgRegNumberSection)
 
-export {CdgUtilitiesDemo, CdgUtilitySection}
+export {
+  CdgUtilitiesDemo,
+  CdgUtilitySection,
+  CdgRegSpaceSection,
+  CdgRegTextSection,
+  CdgRegEmailSection,
+  CdgRegPhoneSection,
+  CdgRegNumberSection,
+}
