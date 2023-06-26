98f2edb7da7ba68736ee7f8a727c41996f13c0f3
fpt-duongtx <duongtx@fpt.com>
2023-06-26 10:32:03 +0700

    Update for skeleton loading layout

--- packages/wc-compass/src/layouts/timeline/timeline-card-time.js
+++ packages/wc-compass/src/layouts/timeline/timeline-card-time.js
@@ -1,13 +1,22 @@
 export class CdgTimelineCardTime extends HTMLElement {
+  textElement
+
   constructor() {
-    super();
+    super()
   }
 
   connectedCallback() {
-    this.classList.add('cdg-timeline-card-time');
-    const icon = document.createElement('cdg-icon');
-    icon.setAttribute('name', 'clock');
-    icon.setAttribute('size', '16');
-    this.prepend(icon);
+    this.classList.add('cdg-timeline-card-time')
+    const text = this.textContent
+    this.textContent = ''
+
+    const icon = document.createElement('cdg-icon')
+    icon.setAttribute('name', 'clock')
+    icon.setAttribute('size', '16')
+    this.appendChild(icon)
+
+    this.textElement = document.createElement('span')
+    this.textElement.textContent = text
+    this.appendChild(this.textElement)
   }
 }
