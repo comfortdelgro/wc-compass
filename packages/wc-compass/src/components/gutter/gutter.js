ab48305979b38b355188f7c559c2aac4795ced89
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-07-03 17:44:10 +0700

    Revert "[yagin][fix gutter can drag over main content]"
    This reverts commit f226341e4d5e8de36811fd836e2b260fcf163bf8.

--- packages/wc-compass/src/components/gutter/gutter.js
+++ packages/wc-compass/src/components/gutter/gutter.js
@@ -28,8 +28,9 @@ export class CdgGutter extends HTMLElement {
   pointer
   capturedSize
   collapseButton
+
   collapsed = false
-  grandparentElement
+
   constructor() {
     super()
   }
@@ -37,8 +38,7 @@ export class CdgGutter extends HTMLElement {
   connectedCallback() {
     this.classList.add('cdg-gutter')
     this.parentElement.classList.add('cdg-contains-gutter')
-    this.grandparentElement = this.parentElement.parentElement
-    this.grandparentElement.style.width = '100%'
+
     if (this.useCollapse) {
       this.attachArrow()
     }
@@ -119,11 +119,7 @@ export class CdgGutter extends HTMLElement {
       (this.place === 'left'
         ? -this.pointer.distance.x
         : this.pointer.distance.x)
-    if (width < 0) {
-      width = 0
-    } else if (width >= this.grandparentElement.offsetWidth) {
-      width = this.grandparentElement.offsetWidth
-    }
+    width = width < 0 ? 0 : width
     this.parentElement.style.width = width + 'px'
     window.dispatchEvent(new Event('resize'))
   }
