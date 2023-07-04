784be1a3249545039f750f600d5bf44229b58e0b
fpt-duongtx <duongtx@fpt.com>
2023-07-04 09:30:44 +0700

    Add new chart

---
+++ apps/docs/public/components/chart/radar-chart/radar-chart.section.js
@@ -0,0 +1,55 @@
+import {CdgBaseComponent} from '../../../shared/base-component'
+import template from './radar-chart.section.html'
+
+export class CdgRadarChartSection extends CdgBaseComponent {
+  constructor() {
+    super()
+    this.template = template
+  }
+
+  onInit() {
+    const data = {
+      labels: [
+        ['Eating', 'Dinner'],
+        ['Drinking', 'Water'],
+        'Sleeping',
+        ['Designing', 'Graphics'],
+        'Coding',
+        'Cycling',
+        'Running',
+      ],
+      datasets: [
+        {
+          data: [20, 40, 60, 70, 10, 54, 3],
+        },
+      ],
+    }
+
+    const config = {
+      type: 'radar',
+      data: data,
+      options: {
+        plugins: {
+          legend: false,
+          tooltip: false,
+        },
+        elements: {
+          line: {
+            backgroundColor: 'rgba(1, 66, 175,0.2)',
+            borderColor: 'rgba(1, 66, 175,0.6)',
+          },
+          point: {
+            backgroundColor: 'rgba(1, 66, 175,0.4)',
+            hoverBackgroundColor: 'rgba(1, 66, 175,0.2)',
+            radius: '5',
+            pointStyle: 'circle',
+            hoverRadius: 1,
+          },
+        },
+      },
+    }
+
+    const chart = this.querySelector('cdg-chart')
+    chart.options = config
+  }
+}
