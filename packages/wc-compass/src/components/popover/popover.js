aa4f3ab7674b3dca112a73f39e481b7abc4b9212
fpt-tom-du <tudq3@fsoft.com.vn>
2023-08-25 11:28:30 +0700

    Add content-wrapper-class to Popover

--- packages/wc-compass/src/components/popover/popover.js
+++ packages/wc-compass/src/components/popover/popover.js
@@ -72,11 +72,15 @@ export class CdgPopover extends CdgBaseComponent {
           this.anchorElement.addEventListener('click', this.clickListener)
         }
       }
+      const floatingClass = this.hasAttribute('content-wrapper-class')
+        ? this.getAttribute('content-wrapper-class').split(',')
+        : []
+      floatingClass.push('cdg-popover-floating-container')
       this.floatingElement = createFloating.bind(this)(
         this.anchorElement,
         this.cdgPopoverContentElement,
         this.direction,
-        ['cdg-popover-floating-container'],
+        floatingClass,
         false,
         false,
         true,
@@ -113,7 +117,7 @@ export class CdgPopover extends CdgBaseComponent {
   }
 
   handleAnchorBlur() {
-    this.open = false
+    // this.open = false
   }
 
   handleKeyboard(event) {
